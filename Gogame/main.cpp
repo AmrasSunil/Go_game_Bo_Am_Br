1be3c56ba06b151779467f2496b6973d7e70ad81
OptionRV <mael.briand@eleves.ec-nantes.fr>
2016-12-08 10:32:59 +0100

    preparation

---
+++ Gogame/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: user
+ *
+ * Created on 8 décembre 2016, 10:30
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
+
